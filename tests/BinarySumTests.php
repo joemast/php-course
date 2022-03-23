f0986af0cf447fc8ef5e61cf2a84d73785b5cd78
Alexander Tarankov <atarankov@plesk.com>
2022-03-24 01:37:57 +0700

    Get rid of import statements

--- tests/BinarySumTests.php
+++ tests/BinarySumTests.php
@@ -1,12 +1,12 @@
 <?php
+declare(strict_types=1);
 
 namespace Tests;
 
 require_once '../src/Trials/BinarySum.php';
-use function Trials\binarySum;
 
-var_dump(binarySum('10', '01'));
-var_dump(binarySum(' 111', '01 '));
-var_dump(binarySum('01', '01'));
-var_dump(binarySum(' 1a1', '01 '));
+var_dump(\Trials\binarySum('10', '01'));
+var_dump(\Trials\binarySum(' 111', '01 '));
+var_dump(\Trials\binarySum('01', '01'));
+var_dump(\Trials\binarySum(' 1a1', '01 '));
 
--- tests/PowerOfThreeTests.php
+++ tests/PowerOfThreeTests.php
@@ -1,18 +1,19 @@
 <?php
+declare(strict_types=1);
 
 namespace Tests;
 
 require_once '../src/Trials/PowerOfThree.php';
-use function Trials\isPowerOfThree;
 
-var_dump(isPowerOfThree(1));
-var_dump(isPowerOfThree(-1));
-var_dump(isPowerOfThree(3));
-var_dump(isPowerOfThree(-3));
-var_dump(isPowerOfThree(-9)); // false because of even power (2)
-var_dump(isPowerOfThree(-27));
-var_dump(isPowerOfThree(-59049)); // false because of even power (10)
-var_dump(isPowerOfThree(59049));
-var_dump(isPowerOfThree(0)); // false
-var_dump(isPowerOfThree(2)); // false
-var_dump(isPowerOfThree(6)); // false
+var_dump(\Trials\isPowerOfThree(1));
+var_dump(\Trials\isPowerOfThree(-1));
+var_dump(\Trials\isPowerOfThree(3));
+var_dump(\Trials\isPowerOfThree(-3));
+var_dump(\Trials\isPowerOfThree(-9)); // false because of even power (2)
+var_dump(\Trials\isPowerOfThree(-27));
+var_dump(\Trials\isPowerOfThree(-59049)); // false because of even power (10)
+var_dump(\Trials\isPowerOfThree(59049));
+var_dump(\Trials\isPowerOfThree(0)); // false
+var_dump(\Trials\isPowerOfThree(2)); // false
+var_dump(\Trials\isPowerOfThree(6)); // false
+var_dump(\Trials\isPowerOfThree(999)); // false
