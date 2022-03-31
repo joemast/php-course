02630e8e04a1e6409990e74321b11b8c21c1f0ab
Alexander Tarankov <atarankov@plesk.com>
2022-04-01 00:20:37 +0700

    AddDigits trial implemented

---
+++ src/Trials/AddDigits.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types=1);
+
+namespace Trials;
+
+class AddDigits
+{
+    public function addDigitsUsingStringConversion(int $number): int
+    {
+        if ($number < 10) {
+            return $number;
+        }
+
+        $numArray = array_map('intval', str_split((string)$number));
+        $res = 0;
+        foreach ($numArray as $digit) {
+            $res += $digit;
+        }
+        return $this->addDigitsUsingStringConversion($res);
+    }
+
+    public function addDigitsUsingTenDivision(int $number): int
+    {
+        if ($number < 10) {
+            return $number;
+        }
+
+        $digitsCount = strlen((string)$number);
+        $res = 0;
+        for ($i = 0; $i < $digitsCount; ++$i) {
+            $res += $number%10;
+            $number = (int)($number/10);
+        }
+
+        return $this->addDigitsUsingTenDivision($res);
+    }
+}
---
+++ tests/AddDigitsTests.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests;
+
+require_once '../src/Trials/AddDigits.php';
+
+$addDigitsObj = new \Trials\AddDigits();
+var_dump($addDigitsObj->addDigitsUsingStringConversion(0));
+var_dump($addDigitsObj->addDigitsUsingStringConversion(1));
+var_dump($addDigitsObj->addDigitsUsingStringConversion(9));
+var_dump($addDigitsObj->addDigitsUsingStringConversion(10));
+var_dump($addDigitsObj->addDigitsUsingStringConversion(128)); // 2
+var_dump($addDigitsObj->addDigitsUsingStringConversion(919)); // 1
+var_dump($addDigitsObj->addDigitsUsingStringConversion(1789)); // 7
+
+var_dump($addDigitsObj->addDigitsUsingTenDivision(0));
+var_dump($addDigitsObj->addDigitsUsingTenDivision(1));
+var_dump($addDigitsObj->addDigitsUsingTenDivision(9));
+var_dump($addDigitsObj->addDigitsUsingTenDivision(10));
+var_dump($addDigitsObj->addDigitsUsingTenDivision(128)); // 2
+var_dump($addDigitsObj->addDigitsUsingTenDivision(919)); // 1
+var_dump($addDigitsObj->addDigitsUsingTenDivision(1789)); // 7
