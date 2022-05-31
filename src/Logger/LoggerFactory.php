c879e80aaf794a55589e10bb798b403c968d6ee4
Alexander Tarankov <atarankov@plesk.com>
2022-06-01 00:08:42 +0700

    Make corrections due to code review comments and linter suggestions

--- src/Logger/LoggerFactory.php
+++ src/Logger/LoggerFactory.php
@@ -7,12 +7,15 @@ class LoggerFactory
     /**
      * @throws \Exception
      */
-    public static function getLogger(array $config): ?LoggerInterface
+    public static function getLogger(array $config): LoggerInterface
     {
         switch ($config['logger']) {
-            case 'console': return new ConsoleLogger();
-            case 'file': return new FileLogger($config['logFileName']);
-            default: throw new \Exception('Logger not configured');
+            case 'console':
+                return new ConsoleLogger();
+            case 'file':
+                return new FileLogger($config['logFileName']);
+            default:
+                throw new \Exception('Logger not configured');
         }
     }
 }
